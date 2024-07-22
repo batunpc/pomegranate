31d797347f55be62ed005701cca41b7f46176c26
batunpc <batuhnpc@gmail.com>
2024-07-22 12:34:45 -0400

    fix: somethn

--- app/albums/[id]/page.tsx
+++ app/albums/[id]/page.tsx
@@ -82,9 +82,9 @@ function ShuffleIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
     >
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M13.484 9.166 15 7h5m0 0-3-3m3 3-3 3M4 17h4l1.577-2.253M4 7h4l7 10h5m0 0-3 3m3-3-3-3"
       />
     </svg>
